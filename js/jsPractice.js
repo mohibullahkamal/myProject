a6cf6816b9d4688064c5f2c0b8d59f0b5da530b6
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-10 18:06:35 +0200

    Practice files on JS

---
+++ js/jsPractice.js
@@ -0,0 +1,9 @@
+alert("Hello from jsPractice.js");
+
+var name = prompt("What is your name?");
+var dob = prompt("What is your Year of Birth?");
+var city = prompt("Which country do you live?");
+
+
+
+
