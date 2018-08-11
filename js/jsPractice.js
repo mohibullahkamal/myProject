4747730437879ab2b9307d6b71fae52a73c19866
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-11 20:51:53 +0200

    Exercise on Birthday and perfect square..done

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -1,9 +1,34 @@
-alert("Hello from jsPractice.js");
+alert("Hello from the other side....");
 
 var name = prompt("What is your name?");
-var dob = prompt("What is your Year of Birth?");
-var city = prompt("Which country do you live?");
+var birth = prompt("What is your Year of Birth?");
+var country = prompt("Which country do you live?");
+
+alert(name + " was born in " + birth + 
+    " and lives in " + country);
 
 
+// *****************************
+// * Birthday + Perfect Square *
+// *****************************
 
+var age = prompt("What is your age, please?");
 
+if (age<0) {
+    alert("Error!!");
+} 
+else if (age == 21) {
+    alert("Happy 21st Birthday!!");
+}
+else if (age % Math.sqrt(age) === 0) {
+    alert("Age is PERFECT SQUARE!!");
+}
+else if (age == 1) { 
+    alert("Your age is odd!!");
+}
+else if (age % 2) {
+    alert("Your age is odd!!");
+}
+else{
+    alert("Your age is: "+age);
+}
