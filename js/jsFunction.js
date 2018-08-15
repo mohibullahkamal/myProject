d25975c2a89d1622ca403321caa258dbf2ea5e01
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-15 18:41:10 +0200

    finished with functions exercise

--- js/jsFunction.js
+++ js/jsFunction.js
@@ -1,12 +1,10 @@
-// ********************
-// ****** isEven ******
-// ********************
-
+// ****************************************
+// ****** isEven **************************
+// ****************************************
 //fast short version...
 function isEven(num) {
     return num % 2 === 0;
 }
-
 //Slow version...
 // function isEven(num) {
 //     //return true if even
@@ -20,10 +18,28 @@ function isEven(num) {
 // }
 
 
-// ********************
-// **** factorial *****
-// ********************
-//factorial(4) --> 4*3*2*1
+// ****************************************
+// **** factorial *************************
+//eg: factorial(4) --> 4*3*2*1      *******
+// ****************************************
+function factorial(num) {
+    //define a result variable
+    var result = 1;
+    //calculate factorial and store value in result
+    for (var i = 2; i <= num; i++) {
+        result = result *= i;
+    }
+    //return the result variable
+    return result;
+}
 
 
-
+// ****************************************
+// ****** kebabToSnake() ******************
+// ****************************************
+function kebabToSnake(str) {
+    //replace all '-' to "_"s
+    var newStr = str.replace(/-/g, "_");
+    //return str
+    return newStr;
+}
