90c1cd37667bb01cbdc25ba467098e508717abb2
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-15 17:47:06 +0200

    added jsFunctions.js

---
+++ js/jsFunction.js
@@ -0,0 +1,29 @@
+// ********************
+// ****** isEven ******
+// ********************
+
+//fast short version...
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+//Slow version...
+// function isEven(num) {
+//     //return true if even
+//     if(num % 2 === 0) {
+//         return true;
+//     }
+//     //return false otherwise
+//     else {
+//         return false;
+//     }
+// }
+
+
+// ********************
+// **** factorial *****
+// ********************
+//factorial(4) --> 4*3*2*1
+
+
+
