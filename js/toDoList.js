cabf2860f7cbc847058571e1cb3030d5b94200af
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-16 12:43:09 +0200

    toDoList error fixed...

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,37 +1,27 @@
-// var todo = ["buy new turtle"];
+var todo = [];
+//first prompt...
+var input = prompt("What would you like to do?"
+                    + "\n'n'  --> Add a Todo"
+                    + "\n'ls' --> List all Todos"
+                    + "\n'q' --> Quit App");
 
-// var input = prompt("What would you like to do? --> "
-//     + "'new' - Add a Todo; 'list' - View all Todos; "
-//     + "'quit' - Quit App");
-
-// while(input !== "quit") {
-//     //handle input
-//     if(input === "list") {
-//         console.log(todo);
-//     } else if (input === "new") {
-//         //ask for new todo
-//         var newTodo = prompt("Enter new Todo :)");
-//         //add to todos array
-//         todo.push(newTodo);
-//     }
-
-//     //ask again for new input
-//     input = prompt("What would you like to do? --> "
-//         + "'new' - Add a Todo; 'list' - View all Todos; "
-//         + "'quit' - Quit App");
-// }
-// console.log("OK ... You are quitting the App!!");
-
-
-
-
-
-
-//empty array
-var todo = ["fsdf"];
-var userInput = prompt("Choose what you want to do: \n[1] 'new'--> Add new todo; \n[2] 'list'--> View all todos; \n[3] 'quit'--> Quit App.")
-
-
+while(input !== "q") {
+    //handle input
+    if(input === "ls") {
+        console.log(todo);
+    } else if (input === "n") {
+        //ask for new entry
+        var newEntry = prompt("Enter new Todo :)");
+        //add to todos array
+        todo.push(newEntry);
+    }
+    //ask again for new input
+    input = prompt("What would you like to do?"
+                    + "\n'n'  --> Add a Todo"
+                    + "\n'ls' --> List all Todos"
+                    + "\n'q' --> Quit App");
+}
+console.log("OK ... You are quitting the App!!");
 
 
 
