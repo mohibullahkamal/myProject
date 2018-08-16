78d6f6781c26f3fe8f15d0f2e01f0c770878b84b
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-16 10:27:01 +0200

    toDoList project complete...

---
+++ js/toDoList.js
@@ -0,0 +1,41 @@
+// var todo = ["buy new turtle"];
+
+// var input = prompt("What would you like to do? --> "
+//     + "'new' - Add a Todo; 'list' - View all Todos; "
+//     + "'quit' - Quit App");
+
+// while(input !== "quit") {
+//     //handle input
+//     if(input === "list") {
+//         console.log(todo);
+//     } else if (input === "new") {
+//         //ask for new todo
+//         var newTodo = prompt("Enter new Todo :)");
+//         //add to todos array
+//         todo.push(newTodo);
+//     }
+
+//     //ask again for new input
+//     input = prompt("What would you like to do? --> "
+//         + "'new' - Add a Todo; 'list' - View all Todos; "
+//         + "'quit' - Quit App");
+// }
+// console.log("OK ... You are quitting the App!!");
+
+
+
+
+
+
+//empty array
+var todo = ["fsdf"];
+var userInput = prompt("Choose what you want to do: \n[1] 'new'--> Add new todo; \n[2] 'list'--> View all todos; \n[3] 'quit'--> Quit App.")
+
+
+
+
+
+
+
+
+
