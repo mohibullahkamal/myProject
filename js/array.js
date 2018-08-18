2c665b6ce697f73d5230130fae37aafe924a2af7
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-18 23:03:35 +0200

    updates on array.js

---
+++ js/array.js
@@ -0,0 +1,25 @@
+//Reverses input
+function printReverse(arr) {
+    for(var i = arr.length - 1; i >= 0; i--) {
+        console.log(arr[i]);
+    }
+}
+printReverse([1,2,3,4,5]);
+
+//checks whether all input equal
+function isUniform(arr) {
+
+}
+isUnifrom([1,2,2,3,1,1,1]);
+
+//sums up all numbers in array
+function sumArray(arr) {
+
+}
+sumArray([2,3,2,3,4]);
+
+//finds max number in array
+function max(arr) {
+
+}
+max([2,5,1,6,9,3]);
