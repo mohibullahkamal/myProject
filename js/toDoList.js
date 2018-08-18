5cd69b40c37a152c157fa362ac388fbd298f6a53
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-18 22:29:45 +0200

    converted all logic into separate functions

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,45 +1,68 @@
-var todo = [];
+var todo = ['Default entry'];
 //first prompt...
-var input = prompt("What would you like to do?"
-                    + "\n'n' --> Add a Todo"
-                    + "\n'ls'--> List all Todo"
-                    + "\n'd' --> Delete a specific Todo"
-                    + "\n'q' --> Quit App");
+userSelection();
 
 while(input !== "q") {
     //handle input
     if(input === "ls") {
-        //start of list
-        console.log("***********");
-        //iterate through todo and list it; "i" represents index of todo
-        todo.forEach(function(myInput, i) {
-            console.log(i + ": " + myInput);
-            // console.log(todo.indexOf(myInput) +
-            //     ": " + myInput);
-        });
-        //end of list
-        console.log("***********");
+        listTodos();
     } else if (input === "n") {
-        //ask for new entry
-        var newEntry = prompt("Enter new Todo :)");
-        //add to todos array
-        todo.push(newEntry);
-        console.log("Added todo :)");
+        addTodo();
     } else if (input === "d") {
-        //ask for index to be deleted
-        var index = prompt("Enter index to delete :)");
-        //delete that todo index; the "1" represents how many element after "index" do you want to do delete
-        todo.splice(index, 1);
-        console.log("Deleted todo :)");
+        deleteTodo();
     }
     //ask again for new input
+    userSelection();
+}
+//quiting App
+console.log("OK ... You are quitting the App!!");
+
+
+
+
+
+
+//************************************* 
+//********** All Functions ************
+//*************************************
+
+function listTodos() {
+    //start of list
+    console.log("***********");
+    //iterate through todo and list it; "i" represents index of todo
+    todo.forEach(function(myInput, i) {
+        console.log(i + ": " + myInput);
+        // console.log(todo.indexOf(myInput) +
+        //     ": " + myInput);
+    });
+    //end of list
+    console.log("***********");
+}
+
+function addTodo() {
+    //ask for new entry
+    var newEntry = prompt("Enter new Todo :)");
+    //add to todos array
+    todo.push(newEntry);
+    console.log("Added todo :)");
+}
+
+function deleteTodo() {
+    //ask for index to be deleted
+    var index = prompt("Enter index to delete :)");
+    //delete that todo index; the "1" represents how many element after "index" do you want to do delete
+    todo.splice(index, 1);
+    console.log("Deleted todo :)");
+}
+
+function userSelection() {
+    //Prompt for user selection
     input = prompt("What would you like to do?"
                     + "\n'n'  --> Add a Todo"
                     + "\n'ls' --> List all Todos"
                     + "\n'd' --> Delete a specific Todo"
                     + "\n'q' --> Quit App");
 }
-//quiting App
-console.log("OK ... You are quitting the App!!");
-
-
+//*************************************
+//*************************************
+//*************************************
