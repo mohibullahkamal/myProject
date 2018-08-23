bc0c74c2bada1f60ccb9e0883689ff10e31232b1
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-23 17:40:30 +0200

    functionality added for buttons - Player1 and 2

--- js/scoreKeeper.js
+++ js/scoreKeeper.js
@@ -1,6 +1,17 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");//just doing it in a different way and mind it we don't need that '#' before id name anymore :)
+var displayP1 = document.querySelector("#p1Display");
+var displayP2 = document.querySelector("#p2Display");
+var p1Score = 0;
+var p2Score = 0;
+
 
 p1Button.addEventListener("click", function(){
-    alert("CLICKED!!");
-})
+    p1Score++;
+    displayP1.textContent = p1Score;
+}); 
+
+p2Button.addEventListener("click", function(){
+    p2Score++;
+    displayP2.textContent = p2Score;
+}); 
