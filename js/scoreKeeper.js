08761b949001eb019f22d5a657274303ee165f43
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-23 13:17:04 +0200

    added eventListener for p1 p2 buttons

--- js/scoreKeeper.js
+++ js/scoreKeeper.js
@@ -1,1 +1,6 @@
-alert("Connected!!");
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById("p2");//just doing it in a different way and mind it we don't need that '#' before id name anymore :)
+
+p1Button.addEventListener("click", function(){
+    alert("CLICKED!!");
+})
