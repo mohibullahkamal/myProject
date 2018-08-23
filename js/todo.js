adbbc77406e518a376bac49a469fbcd1d25837bc
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-23 21:15:41 +0200

    added todo.css

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,14 @@
 // var firstLi = document.querySelector("li");
 var lis = document.querySelectorAll("li");
 
-lis.addEventListener("mouseover", function() {
-
-});
+//Loops through all lis...
+for(var i = 0; i < lis.length; i++) {
+    lis[i].addEventListener("mouseover", function() { 
+        this.style.color = "red";
+        this.style.fontWeight = "bold"; //bolds font on hover
+    });
+    lis[i].addEventListener("mouseout", function() {
+        this.style.color = "black";
+        this.style.fontWeight = "normal"; //changes it back to normal on mouseOut
+    });
+}
