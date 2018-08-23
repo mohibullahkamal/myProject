9432cd57c06ec1e2ac1a93fa8adf376d1f5a5907
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-23 21:31:12 +0200

    finished todo project

--- js/todo.js
+++ js/todo.js
@@ -4,11 +4,14 @@ var lis = document.querySelectorAll("li");
 //Loops through all lis...
 for(var i = 0; i < lis.length; i++) {
     lis[i].addEventListener("mouseover", function() { 
-        this.style.color = "red";
-        this.style.fontWeight = "bold"; //bolds font on hover
+        // this.style.color = "red"; // we are not using this, but instead we are using the below... for separation of concern
+        this.classList.add("selected");
     });
     lis[i].addEventListener("mouseout", function() {
-        this.style.color = "black";
-        this.style.fontWeight = "normal"; //changes it back to normal on mouseOut
+        // this.style.color = "black"; // we are not using this, but instead we are using the below... for separation of concern
+        this.classList.remove("selected"); // using ".remove" to remove "selected" class
+    });
+    lis[i].addEventListener("click", function(){
+        this.classList.toggle("done");
     });
 }
