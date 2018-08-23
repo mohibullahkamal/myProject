12377d2498012bfdd5aa2e3f8be9ff0c3b7ca62f
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-23 20:54:46 +0200

    added todo.js and todo.html

--- js/scoreKeeper.js
+++ js/scoreKeeper.js
@@ -1,6 +1,6 @@
 // *********** Buttons ***********
 var p1Button = document.querySelector("#p1");
-var p2Button = document.getElementById("p2");//just doing it in a different way and mind it we don't need that '#' before id name anymore :)
+var p2Button = document.getElementById("p2"); //just doing it in a different way and mind it we don't need that '#' before id name anymore :)
 var resetButton = document.getElementById("reset");
 
 // *** variables used in file ***
---
+++ js/todo.js
@@ -0,0 +1,6 @@
+// var firstLi = document.querySelector("li");
+var lis = document.querySelectorAll("li");
+
+lis.addEventListener("mouseover", function() {
+
+});
