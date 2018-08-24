ee08aa99a509d0ff543b486a3ad2a7d0b388cd08
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-24 19:37:09 +0200

    added rgb index to heading

--- js/colorGame.js
+++ js/colorGame.js
@@ -8,9 +8,14 @@ var colors = [
     "rgb(255, 0, 255)"
 ]
 
-//lets loop through colors
+// ****** All Variables ******
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var display = document.getElementById("colorDisplay");
+display.textContent = pickedColor;
+// ***************************
 
+// lets loop through colors
 for(var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
 }
