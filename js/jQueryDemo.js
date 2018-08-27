1afc6b197c7fe06b5f012493ca2143c0d621143e
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-27 23:01:52 +0200

    first jQuery exercise completed

---
+++ js/jQueryDemo.js
@@ -0,0 +1,11 @@
+// all div's purple background...
+$("div").css("backgroundColor", "purple");
+
+//div with "highlight"=class make them 200px wide
+$(".highlight").css("width", "200px");
+
+//id "third" and give it orange border
+$("#third").css("border", "2px dashed orange");
+
+//select first div only and change the fontColor to pink
+$("div:first").css("color", "pink");
