e5b78343dcf67cd3c9bb73a1976d7ced15346f66
mogit2020 <mohibullah.kamal@gmail.com>
2018-08-31 23:26:46 +0200

    Uodate

--- js/colorGame.js
+++ js/colorGame.js
@@ -124,6 +124,15 @@ function generateRandomColors(num) {
     return arr;
 }
 
-
+//randomColor generator
+function randomColor() {
+    //pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    //pick a "green" from 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    //pick a "blue" from 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    //return rgb(r,g,b)
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 // ******************************************************
