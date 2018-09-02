67eed54c7ded3724e4f9babb8fd69bce31272174
mogit2020 <mohibullah.kamal@gmail.com>
2018-09-02 16:29:13 +0200

    toggle() makes life easy

--- jQueryTodoProject/assets/js/jQueryTodo.js
+++ jQueryTodoProject/assets/js/jQueryTodo.js
@@ -1,12 +1,5 @@
 //Check Off Specific Todos By Clicking
 $("li").click(function() {
     //if statement checks for "click" state
-    if( $(this).css("color") === "grey") {
-        console.log("IT is Currently grey..");
-    }
-
-    //turn text to grey and line through
-    $(this).css("color", "grey");
-    $(this).css("text-decoration", 
-        "line-through");
+    $(this).toggleClass("completed");
 });
