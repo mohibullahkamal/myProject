bc32a463d9aa139393baea9a11ba573ac81d6d02
mogit2020 <mohibullah.kamal@gmail.com>
2018-09-02 23:42:42 +0200

    bug fixes jQueryTodo

--- jQueryTodoProject/assets/js/jQueryTodo.js
+++ jQueryTodoProject/assets/js/jQueryTodo.js
@@ -22,7 +22,7 @@ $("input[type='text']").keypress(function(event) {
         var todoText = $(this).val();
         $(this).val("");
         //create a new li and add to ul
-        $("ul").append("<li><span><i class='fa fa-trash'></i></span> "+
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+
             todoText +"</li>")
     }
 });
