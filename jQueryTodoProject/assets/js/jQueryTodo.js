3a225a86600d71c6e83c39cc6aee4ae00f32292d
mogit2020 <mohibullah.kamal@gmail.com>
2018-09-02 17:28:41 +0200

    finished delete and fadeOut feature

--- jQueryTodoProject/assets/js/jQueryTodo.js
+++ jQueryTodoProject/assets/js/jQueryTodo.js
@@ -1,5 +1,20 @@
 //Check Off Specific Todos By Clicking
 $("li").click(function() {
-    //if statement checks for "click" state
+    //Toggles from on and off state
     $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("span").click(function(event) {
+    //setting 500ms for fading out; and then removing it out
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    //stopPropogation stops the general problem of "Bubble Effect" which is when <span> is clicked all parents upto head is fired respectively... we dont want that
+    event.stopPropagation();
+});
+
+//Create new Todos
+$("input[type='text']").keypress(function() {
+    console.log("hrhrhrhr");
 });
