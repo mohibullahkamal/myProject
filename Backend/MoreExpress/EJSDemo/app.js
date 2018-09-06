825f8c49d560f1004cd8810ba096c5a7fe0f2809
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-06 16:15:00 +0000

    Working on advanced express

--- Backend/MoreExpress/EJSDemo/app.js
+++ Backend/MoreExpress/EJSDemo/app.js
@@ -2,11 +2,19 @@ var express = require("express");
 var app     = express();
 
 app.get("/", function(req, res) {
-    res.send("Welcome to the HomePage...!!")
+    res.render("home.ejs"); // instead of using "res.send()" .....   .ejs(Embedded JS) It can run .html files ..
 });
 
+app.get("/iloveu/:thing", function(req, res) {
+   var name = req.params.thing.toUpperCase();
+   res.render("love.ejs");
+});
+
+
+
+
+
+// Port variables that c9 has set for their environment...
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started...");
 });
-
-
