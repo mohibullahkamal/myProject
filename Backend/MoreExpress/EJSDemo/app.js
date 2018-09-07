3103a674c3948848122ab1c9adaa6efbdc4187f0
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-07 20:23:52 +0000

    EJS conditions and loops done

--- Backend/MoreExpress/EJSDemo/app.js
+++ Backend/MoreExpress/EJSDemo/app.js
@@ -6,22 +6,31 @@ app.get("/", function(req, res) {
     res.render("home.ejs"); // instead of using "res.send()" .....   .ejs(Embedded JS) It can run .html files ..
 });
 
-app.get("/iloveu/:thing", function(req, res) {
+app.get("/like/:thing", function(req, res) {
    var name = req.params.thing;
-   console.log("User loves --> " + name);
+   console.log("User likes --> " + name);
    res.render("love.ejs", {nameVar: name}); // {}-this is an object... so basically we are passing nameVar through an object... so it gets identified by love.ejs file...
 });
 
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My life story", author: "James"},
+        {title: "WebDev", author: "Mohib"}
+    ];
+    console.log("User is looking for Posts");
+    res.render("posts.ejs", {posts: posts});
+});
 
 
 
 
 // Error input..
 app.get("*", function(req, res){
-   console.log("SOMEONE MADE A REQUEST TO *..!!");
+   console.log("SOMEONE MADE A  WRONG REQUEST!!");
    res.send("Please enter correct URL..!!");
-})
+});
 
 // Port variables that c9 has set for their environment...
 app.listen(process.env.PORT, process.env.IP, function(){
