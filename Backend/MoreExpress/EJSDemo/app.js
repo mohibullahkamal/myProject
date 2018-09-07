c40fe3d84d813bded42d0cd1922f3ff102f1662e
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-07 08:34:58 +0000

    Update

--- Backend/MoreExpress/EJSDemo/app.js
+++ Backend/MoreExpress/EJSDemo/app.js
@@ -2,17 +2,26 @@ var express = require("express");
 var app     = express();
 
 app.get("/", function(req, res) {
+    console.log("User requested HomePage");
     res.render("home.ejs"); // instead of using "res.send()" .....   .ejs(Embedded JS) It can run .html files ..
 });
 
 app.get("/iloveu/:thing", function(req, res) {
-   var name = req.params.thing.toUpperCase();
-   res.render("love.ejs");
+   var name = req.params.thing;
+   console.log("User loves --> " + name);
+   res.render("love.ejs", {nameVar: name}); // {}-this is an object... so basically we are passing nameVar through an object... so it gets identified by love.ejs file...
 });
 
 
 
 
+
+
+// Error input..
+app.get("*", function(req, res){
+   console.log("SOMEONE MADE A REQUEST TO *..!!");
+   res.send("Please enter correct URL..!!");
+})
 
 // Port variables that c9 has set for their environment...
 app.listen(process.env.PORT, process.env.IP, function(){
