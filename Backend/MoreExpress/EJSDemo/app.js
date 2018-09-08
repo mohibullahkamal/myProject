f929ebf6a3afb421f0d4afcdd12c23a7ee85d26d
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-08 09:50:03 +0000

    Completed the use of 'app.use(express static ...)'

--- Backend/MoreExpress/EJSDemo/app.js
+++ Backend/MoreExpress/EJSDemo/app.js
@@ -1,17 +1,25 @@
 var express = require("express");
 var app     = express();
 
+//express does not directly use resources from your directory... you have to explicitly ask for it and assign it
+app.use(express.static("public")); // "public" is the name for directory... that we want to connect to... we don't need to mention in the app.css file... we can just simple call it...
+
+// with this command we now no longer have to write home.ejs; love.ejs; posts.ejs  ..... to shorten our code...
+app.set("view engine", "ejs");
+
+
 app.get("/", function(req, res) {
     console.log("User requested HomePage");
-    res.render("home.ejs"); // instead of using "res.send()" .....   .ejs(Embedded JS) It can run .html files ..
+    res.render("home"); // instead of using "res.send()" .....   .ejs(Embedded JS) It can run .html files ..
 });
 
 app.get("/like/:thing", function(req, res) {
    var name = req.params.thing;
    console.log("User likes --> " + name);
-   res.render("love.ejs", {nameVar: name}); // {}-this is an object... so basically we are passing nameVar through an object... so it gets identified by love.ejs file...
+   res.render("love", {nameVar: name}); // {}-this is an object... so basically we are passing nameVar through an object... so it gets identified by love.ejs file...
 });
 
+// posts ...
 app.get("/posts", function(req, res){
     var posts = [
         {title: "Post 1", author: "Susy"},
@@ -19,7 +27,7 @@ app.get("/posts", function(req, res){
         {title: "WebDev", author: "Mohib"}
     ];
     console.log("User is looking for Posts");
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts}); // the first "posts" must have been "posts.ejs" but we can shorten it ...lol... to do this we need to set it above... "app.set("view engine") "....
 });
 
 
