a0e700671e47ce81b3b39a538225ae599d6dba08
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-08 13:33:19 +0000

    List of friends displayed for PostRequestDemo

--- Backend/MoreExpress/PostRequestDemo/app.js
+++ Backend/MoreExpress/PostRequestDemo/app.js
@@ -4,24 +4,17 @@ var app = express();
 app.set("view engine", "ejs");
 
 
-
-
-
-
 app.get("/", function(req, res) {
-    console.log("User requested Home...")
+    console.log("User requested Home...");
     res.render("home");
-})
+});
 
-app.get("/", function(req, res) {
-    console.log("User requested Home...")
-    res.render("home");
-})
+app.get("/friends", function(req, res) {
+    var friends = ["Tony", "Miranda", "Justin", "MOHIB"];
+    console.log("User requested Friend List...");
+    res.render("friends", {friends: friends});
+});
 
-app.get("/", function(req, res) {
-    console.log("User requested Home...")
-    res.render("home");
-})
 
 
 
@@ -30,9 +23,9 @@ app.get("/", function(req, res) {
 
 // home 
 app.get("/", function(req, res) {
-    console.log("User requested Home...")
+    console.log("User requested Home...");
     res.render("home");
-})
+});
 
 // listen port
 app.listen(process.env.PORT, process.env.IP, function() {
