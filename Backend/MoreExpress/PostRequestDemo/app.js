0955bcc3c95f9bec0b3c81506e1ac126ac9433d6
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-08 12:38:33 +0000

    All basic file structures setup for PostRequestDemo project

--- Backend/MoreExpress/PostRequestDemo/app.js
+++ Backend/MoreExpress/PostRequestDemo/app.js
@@ -2,3 +2,39 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
+
+
+
+
+
+
+app.get("/", function(req, res) {
+    console.log("User requested Home...")
+    res.render("home");
+})
+
+app.get("/", function(req, res) {
+    console.log("User requested Home...")
+    res.render("home");
+})
+
+app.get("/", function(req, res) {
+    console.log("User requested Home...")
+    res.render("home");
+})
+
+
+
+
+
+
+// home 
+app.get("/", function(req, res) {
+    console.log("User requested Home...")
+    res.render("home");
+})
+
+// listen port
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server Started !!");
+});
