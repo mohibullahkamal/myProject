81cbd9a4d7ff73ab5da414c2cc0f0354ae56b842
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-09 12:32:47 +0000

    HTML layout created to display results

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -3,11 +3,16 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
+
 
 app.get("/results", function(req, res) {
-    res.send("Hello, it works!");
     request("http://www.omdbapi.com/?apikey=3f2f6179&&&s=fast", function(error, response, body) {
-        if
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.render("render", {data: data});
+            //res.send(results["Search"][0]["Title"]); // remember res.send can only be used once...
+        }
     });
 });
 
