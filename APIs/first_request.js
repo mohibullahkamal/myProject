0d013a822ade0e065df35306ba27b75ea3d08a6d
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-09 10:18:19 +0000

    install all dependencies for movie_search_app

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,10 +1,9 @@
 
 var request = require('request');
-console.log("Sunset in Hawaii is at...");
 request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function (error, response, body) {
     if(!error && response.statusCode == 200) {
         var parsedData = JSON.parse(body); // when we request for weather data we get strings of data... and we need to parse it... luckily JS comes with JSON.parse....
-        console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]); // but the data is still printed like an object... with curly brackets... so to only get time of sunset we use "[]"...
+        console.log("Sunset in Hawaii is at -> " + parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]); // but the data is still printed like an object... with curly brackets... so to only get time of sunset we use "[]"...
     }
 });
     
