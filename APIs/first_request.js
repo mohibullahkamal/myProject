f0c4025c3568d4e4c2acab027687782845cfd161
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-09 09:52:27 +0000

    get sunset time for Hawaii using yahoo weather API

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,4 +1,30 @@
 
+var request = require('request');
+console.log("Sunset in Hawaii is at...");
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function (error, response, body) {
+    if(!error && response.statusCode == 200) {
+        var parsedData = JSON.parse(body); // when we request for weather data we get strings of data... and we need to parse it... luckily JS comes with JSON.parse....
+        console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]); // but the data is still printed like an object... with curly brackets... so to only get time of sunset we use "[]"...
+    }
+});
+    
+
+
+
+// // My own code... similar to the one above
+// var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//     if(error) {
+//         console.log("SOMETHING WENT WRONG !!");
+//         console.log(error);
+//     } else {
+//         if(response.statusCode == 200) {
+//             //things worked !
+//             console.log(body);
+//         }
+//     }
+// });
+
 // Code copied from github-request....
 // var request = require('request');
 // request('http://www.google.com', function (error, response, body) {
@@ -8,16 +34,19 @@
 // });
 
 
-// My own code... similar to the one above
-var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-    if(error) {
-        console.log("SOMETHING WENT WRONG !!");
-        console.log(error);
-    } else {
-        if(response.statusCode == 200) {
-            //things worked !
-            console.log(body);
-        }
-    }
-});
+
+
+// {
+//  "query": {
+//   "count": 1,
+//   "created": "2018-09-09T08:38:26Z",
+//   "lang": "en-US",
+//   "results": {
+//   "channel": {
+//     "astronomy": {
+//      "sunset": "6:35 pm"
+//     }
+//   }
+//   }
+//  }
+// }
