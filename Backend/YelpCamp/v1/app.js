dd97aab5a3d71f620f38d24173562412a8b7ef63
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-09-10 10:10:46 +0000

    Landing page added

--- Backend/YelpCamp/v1/app.js
+++ Backend/YelpCamp/v1/app.js
@@ -1,10 +1,35 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res){
-    res.send();
-})
+app.set("view engine", "ejs");
 
+
+// home page
+app.get("/", function(req, res){
+    console.log("User Requested HOME page.");
+    // res.send("this will be the landing page soon...!!"); //used to test server connection... abondoned it for res.render("landing")...
+    res.render("landing");
+});
+
+// redirect to "/campground" page
+app.get("/campground", function(req, res){
+    console.log("User Requested /CAMPGROUND page.");
+
+});
+
+
+
+
+
+
+
+// Error page
+app.get("*", function(req, res){
+   console.log("User Entered wrong URL..!!");
+   res.send("Please Enter Corret URL.");
+});
+
+// Server setup for c9
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started !!!");
 });
