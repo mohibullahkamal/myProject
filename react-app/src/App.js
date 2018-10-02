29fd51484d13e4c500ae4816d885879952ec8c75
mogit2020 <mohibullah.kamal@gmail.com>
2018-10-02 22:51:23 +0200

    add props to App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working!!</p>
-        <Person />   {/*this is from Person.js*/} 
+        <Person name = "Ron" age = "27" />   {/*this is from Person.js*/} 
+        <Person name = "Sheldon" age = "44" /> 
+        <Person name = "Hermoyni" age = "32">My Hobby is coding</Person> 
+        <Person name = "Elizabeth" age = "34" />  
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now??'));   //This is the JS version of the above JSX
@@ -16,5 +19,4 @@ class App extends Component {
 }
 
 export default App;
-
 
--- react-app/src/Person/Person.js
+++ react-app/src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 
-const person = () => {
-    return <p>I am a Person !!!</p>
+const person = (props) => {
+    return <p>I am {props.name} and I am {props.age/*Math.floor(Math.random()*30)*/} years old !!!</p> //allows us to enter dynamic content...
 }
 
 export default person;
