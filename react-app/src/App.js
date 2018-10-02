95528761bd0f1a7cd9c2b4ce496369b18f97d68e
mogit2020 <mohibullah.kamal@gmail.com>
2018-10-02 21:28:19 +0200

    Create Person.js file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I am a React App</h1>
-    //   </div>
-    // );
-
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now??'));
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <p>This is really working!!</p>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now??'));
   }
 }
 
---
+++ react-app/src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const person = () => {
+    return (
+        <div>
+            <p>
+                I am a Person !!!
+            </p>
+        </div>
+    );
+}
+
+export default person;
+
+
