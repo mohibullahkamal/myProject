01f4bfce1a6170f59d1b744f3785c5e536e4c927
mogit2020 <mohibullah.kamal@gmail.com>
2018-10-02 19:03:30 +0200

    modify App.js file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, I am a React App</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I am a React App</h1>
+    //   </div>
+    // );
+
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now??'));
   }
 }
 
