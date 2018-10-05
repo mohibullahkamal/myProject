1cb0a8cadd22cd903785c87169ce65e7ad82798c
mogit2020 <mohibullah.kamal@gmail.com>
2018-10-05 22:54:52 +0200

    Persisting original state of binding

--- react-app/src/Person/Person.js
+++ react-app/src/Person/Person.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-
 const person = (props) => {
     return (
         <div>
             <p onClick={props.clickABC}>I am {props.name} and I am {props.age/*Math.floor(Math.random()*30)*/} years old !!!</p>  
             <p>{props.children}</p>   {/*children is a reserved word in React -> refers to anyword or elements between openning and closing tags of our component; We could nest complex html code too...other React components... could be unordered lists with list items... */}
-            <input type="text" onChange={props.changed}/>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 }
