dbe8e6a0db006a8500773b94d346df311c228b35
mogit2020 <mohibullah.kamal@gmail.com>
2018-10-06 17:41:10 +0200

    Output a list and use .map()

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -55,17 +55,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person 
-            name = {this.state.persons[0].name} 
-            age = {this.state.persons[0].age}/>
-          <Person 
-            name = {this.state.persons[1].name} 
-            age = {this.state.persons[1].age} 
-            clickABC = {this.switchNameHandler.bind(this, 'Max!!')}
-            changed = {this.nameChangedHandler}> My Hobbies: Racing</Person>
-          <Person 
-            name = {this.state.persons[2].name}
-            age = {this.state.persons[2].age}/>
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name} 
+              age={person.age} />
+          })}   {/*   .map() gives us a way to map simply maps every element in a given array into something else... It does this by executing a method on every element on a given array... */}
         </div>
       );
     }
